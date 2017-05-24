4c50a565d23dd4dd6b731d21b5774c50189d097a
Syrimande <caterinadasilva@gmail.com>
2017-05-24 10:01:48 -0400

    Links a Redes sociales en footer, integración de Juego: creación de tablero

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 function validateForm() {
   document.getElementById("driverBtn").addEventListener("click", function(){
-    console.log('Driver number: ' + document.getElementById('driverNumber').value)
+  console.log('Driver number: ' + document.getElementById('driverNumber').value);
     if (document.getElementById('driverNumber').value != "") {
-    alert('Great! We will send you a message to your phone with the instructions.')
-  } else {
-    alert('Please, insert a valid phone number.')
-  }
-});
+      alert('Great! We will send you a message to your phone with the instructions.');
+    } else {
+      alert('Please, insert a valid phone number.');
+    }
+  });
+  event.preventDefault();
 }
 
 function game() {
@@ -20,26 +21,40 @@ function game() {
     [0,0,0,0,0,0,0,0,0,0]
   ]
 
-  //Ingreso el auto en la posicion 3,5
-  tablero[3][5] = "A";
-
   //Obtengo el div en donde se dibujará el tablero
   var divTablero = document.getElementById("game");
-
+  //Obtengo las coordenadas
+  /*var xCoordinate = parseInt(document.getElementById('xCoordinate'));
+  var yCoordinate = parseInt(document.getElementById('yCoordinate'));*/
   //Variables temporales
   var fila, casilla;
 
-
-  //Recorro el arreglo para dibujar el tablero
-  for(var i = 0; i<tablero.length; i++){
-    fila = document.createElement("div");
-    fila.classList.add("fila");
-    for(var j = 0; j<tablero[i].length; j++){
-      casilla = document.createElement("div");
-      casilla.innerHTML = tablero[i][j];
-      casilla.classList.add("casilla")
-      fila.appendChild(casilla);
-    }
-    divTablero.appendChild(fila);
-  }
+  document.getElementById("startBtn").addEventListener("click", function(){
+    event.preventDefault();
+    var xCoordinate = document.getElementById('xCoordinate').value;
+    var yCoordinate = document.getElementById('yCoordinate').value;
+    console.log('X Coordinate: ' + xCoordinate);
+    console.log('Y Coordinate: ' + yCoordinate);
+    if (document.getElementById('xCoordinate').value == "") {
+      alert('Please, insert a number between 0 and 9 in the X Coordinate.')
+    } else if (document.getElementById('yCoordinate').value == "") {
+        alert('Please, insert a number between 0 and 5 in the Y Coordinate.')
+    } else {
+      document.getElementById("game").innerHTML = "";
+      //Recorro el arreglo para dibujar el tablero
+      for(var i = 0; i<tablero.length; i++){
+        fila = document.createElement("div");
+        fila.classList.add("fila");
+        for(var j = 0; j<tablero[i].length; j++){
+          casilla = document.createElement("div");
+          casilla.innerHTML = tablero[i][j];
+          casilla.classList.add("casilla")
+          fila.appendChild(casilla);
+        }
+        divTablero.appendChild(fila);
+      }
+      //Ingreso el auto en la posicion ingresada en los input
+      tablero[document.getElementById('xCoordinate').value][document.getElementById('yCoordinate').value] = "A";
+      }
+    });
 }
